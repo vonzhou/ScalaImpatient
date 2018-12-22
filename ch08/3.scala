303aa1b9f651213b46914a0ef7be2d99bdaa1213
vonzhou <vonzhou@163.com>
2018-12-22 21:19:32 +0800

    chapter 8 ex

---
+++ ch08/3.scala
@@ -0,0 +1,48 @@
+/**
+  * Created by vonzhou on 2018/12/22.
+  */
+
+import collection.immutable.List
+
+abstract class Shape {
+  val name = "Abstract shape"
+
+  override def toString = name
+}
+
+trait Drawable {
+  def draw() = println(toString)
+}
+
+class Point(val x: Int, val y: Int) {
+  override def toString = "%d,%d".format(x, y)
+}
+
+object Point {
+  def apply(x: Int = 0, y: Int = 0) = new Point(x, y)
+}
+
+class Rectangle(val topLeft: Point, val bottomRight: Point) extends Shape with Drawable {
+  override val name = "Rectangle"
+
+  override def toString = {
+    "%s (%s - %s)".format(name, topLeft.toString, bottomRight.toString)
+  }
+}
+
+class Circle(val center: Point, val radius: Int) extends Shape with Drawable {
+  override val name = "Circle"
+
+  override def toString = {
+    "%s (%s: %d)".format(name, center.toString, radius)
+  }
+}
+
+val l: List[Shape with Drawable] = List(
+  new Rectangle(Point(1, 1), Point(10, 20)),
+  new Circle(Point(2, 2), 5)
+)
+
+for (x <- l) x.draw()
+
+
---
+++ ch08/5.scala
@@ -0,0 +1,19 @@
+/**
+  * Created by vonzhou on 2018/12/22.
+  */
+class Point(val x: Int, val y: Int) {
+  override def toString = "Point(%d, %d)".format(x, y)
+}
+
+class LabeledPoint(val label: String, x: Int, y: Int) extends Point(x, y) {
+  override def toString = {
+    "LabeledPoint(%s, %d, %d)".format(label, x, y)
+  }
+}
+
+val a = new Point(1, 1)
+println(a)
+
+val b = new LabeledPoint("Some point", 20, 50)
+println(b)
+
