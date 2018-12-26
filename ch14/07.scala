fb6f39f6c7b69de969227a471541e6061be97037
vonzhou <vonzhou@163.com>
2018-12-26 19:31:05 +0800

    chapter 14 习题

---
+++ ch14/07.scala
@@ -0,0 +1,15 @@
+sealed abstract class BinaryTree
+
+case class Leaf(value: Int) extends BinaryTree
+
+// 每个节点可以有多个子节点
+case class Node(leafs: BinaryTree*) extends BinaryTree
+
+def leafSum(tree: BinaryTree): Int = tree match {
+  case Node(leafs@_*) => leafs.map(leafSum _).sum
+  case Leaf(x) => x
+}
+
+val x = Node(Node(Leaf(3), Leaf(8)), Leaf(2), Node(Leaf(5)))
+println(x)
+println(leafSum(x))
